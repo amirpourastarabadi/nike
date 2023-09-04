f9432758ad4dd56d89493966f13aa2e2e2f93b4b
amirpourastarabadi <amirpourastarabadi@gmail.com>
2023-09-04 13:44:47 +0330

    imp subscription

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,6 +4,7 @@ const Button = ({
   backgroundColor,
   textColor,
   borderColor,
+  fullWidth
 }) => {
   return (
     <button
@@ -15,7 +16,7 @@ const Button = ({
           backgroundColor
             ? `${backgroundColor} ${textColor} ${borderColor}`
             : "bg-coral-red text-white border-coral-red"
-        }`}
+        } ${fullWidth && "w-full"}`}
     >
       {label}
       {iconURL && (
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,7 +1,35 @@
+import Button from "../components/Button"
+
 const Subscribe = () => {
-    return (
-        <div>Subscribe</div>
-    )
+  return (
+    <section
+      className="flex justify-between items-center 
+        max-lg:flex-col gap-10 max-container"
+    >
+      <h3
+        className="font-bold text-4xl 
+            leading-[68px] lg:max-w-md font-palanquin"
+      >
+        Sign Up for
+        <span className="text-coral-red"> Updates </span>& Newsletter
+      </h3>
+
+      <div
+        className="
+            lg:max-w-[50%] w-full flex items-center
+            max-sm:flex-col gap-5 p-2.5 sm:border
+            sm:border-slate-gray rounded-full"
+      >
+        <input type="text" placeholder="subscribe@nike.com" className="input" />
+        <div
+          className="flex max-sm:justify-end items-center
+        max-sm:w-full"
+        >
+          <Button label="Subscribe" fullWidth/>
+        </div>
+      </div>
+    </section>
+  )
 }
 
-export default Subscribe;
+export default Subscribe
